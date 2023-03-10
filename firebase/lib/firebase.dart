615ea1858dac434f95337a6118f51cd56b78091a
Wojtas399 <wojtekp26@gmail.com>
2023-03-10 22:48:56 +0100

    Some corrections in firebase folder.

--- firebase/lib/firebase.dart
+++ firebase/lib/firebase.dart
@@ -2,10 +2,15 @@ library firebase;
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 export 'package:firebase_auth/firebase_auth.dart';
 
-part 'fire_collections.dart';
+part 'firebase_collections.dart';
 part 'model/userDto.dart';
 part 'service/firebase_auth_service.dart';
 part 'service/firebase_user_service.dart';
+
+Future<void> initializeFirebaseApp() async {
+  await Firebase.initializeApp();
+}
