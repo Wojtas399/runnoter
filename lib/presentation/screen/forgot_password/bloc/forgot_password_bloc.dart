5b972480fcf84c9086fc61e5baaf4cef013ef4c6
Wojtas399 <wojtekp26@gmail.com>
2023-03-17 17:42:39 +0100

    Connectivity checking in forgot password bloc.

--- lib/presentation/screen/forgot_password/bloc/forgot_password_bloc.dart
+++ lib/presentation/screen/forgot_password/bloc/forgot_password_bloc.dart
@@ -4,18 +4,22 @@ import '../../../../domain/model/auth_exception.dart';
 import '../../../../domain/service/auth_service.dart';
 import '../../../model/bloc_status.dart';
 import '../../../model/bloc_with_status.dart';
+import '../../../service/connectivity_service.dart';
 import 'forgot_password_event.dart';
 import 'forgot_password_state.dart';
 
 class ForgotPasswordBloc extends BlocWithStatus<ForgotPasswordEvent,
     ForgotPasswordState, ForgotPasswordInfo, ForgotPasswordError> {
   final AuthService _authService;
+  final ConnectivityService _connectivityService;
 
   ForgotPasswordBloc({
     required AuthService authService,
+    required ConnectivityService connectivityService,
     BlocStatus status = const BlocStatusInitial(),
     String email = '',
   })  : _authService = authService,
+        _connectivityService = connectivityService,
         super(
           ForgotPasswordState(
             status: status,
@@ -39,18 +43,44 @@ class ForgotPasswordBloc extends BlocWithStatus<ForgotPasswordEvent,
     ForgotPasswordEventSubmit event,
     Emitter<ForgotPasswordState> emit,
   ) async {
+    emitLoadingStatus(emit);
+    if (await _connectivityService.hasDeviceInternetConnection() == false) {
+      emitNoInternetConnectionStatus(emit);
+      return;
+    }
     try {
-      emitLoadingStatus(emit);
-      await _authService.sendPasswordResetEmail(
-        email: state.email,
-      );
+      await _tryToSendPasswordResetEmail();
       emitCompleteStatus(emit, ForgotPasswordInfo.emailSubmitted);
     } on AuthException catch (authException) {
-      if (authException == AuthException.invalidEmail) {
-        emitErrorStatus(emit, ForgotPasswordError.invalidEmail);
-      } else if (authException == AuthException.userNotFound) {
-        emitErrorStatus(emit, ForgotPasswordError.userNotFound);
+      final ForgotPasswordError? error = _mapAuthExceptionToBlocError(
+        authException,
+      );
+      if (error != null) {
+        emitErrorStatus(emit, error);
+      } else {
+        emitUnknownErrorStatus(emit);
+        rethrow;
       }
+    } catch (_) {
+      emitUnknownErrorStatus(emit);
+      rethrow;
     }
+  }
+
+  Future<void> _tryToSendPasswordResetEmail() async {
+    await _authService.sendPasswordResetEmail(
+      email: state.email,
+    );
+  }
+
+  ForgotPasswordError? _mapAuthExceptionToBlocError(
+    AuthException exception,
+  ) {
+    if (exception == AuthException.invalidEmail) {
+      return ForgotPasswordError.invalidEmail;
+    } else if (exception == AuthException.userNotFound) {
+      return ForgotPasswordError.userNotFound;
+    }
+    return null;
   }
 }
--- lib/presentation/screen/forgot_password/ui/forgot_password_screen.dart
+++ lib/presentation/screen/forgot_password/ui/forgot_password_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../../domain/service/auth_service.dart';
 import '../../../component/bloc_with_status_listener_component.dart';
+import '../../../service/connectivity_service.dart';
 import '../../../service/dialog_service.dart';
 import '../../../service/navigator_service.dart';
 import '../bloc/forgot_password_bloc.dart';
@@ -37,6 +38,7 @@ class _BlocProvider extends StatelessWidget {
     return BlocProvider(
       create: (_) => ForgotPasswordBloc(
         authService: context.read<AuthService>(),
+        connectivityService: ConnectivityService(),
       ),
       child: child,
     );
