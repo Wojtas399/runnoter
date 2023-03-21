574f4abf34063711f2fd3eadcc037278cf36f3fc
Wojtas399 <wojtekp26@gmail.com>
2023-03-21 22:12:55 +0100

    State repository model.

---
+++ lib/domain/model/state_repository.dart
@@ -0,0 +1,34 @@
+import 'package:rxdart/rxdart.dart';
+
+import 'entity.dart';
+
+class StateRepository<T extends Entity> {
+  final BehaviorSubject<List<T>?> _dataStream =
+      BehaviorSubject<List<T>?>.seeded(null);
+
+  StateRepository({List<T>? initialData}) {
+    _dataStream.add(initialData);
+  }
+
+  Stream<List<T>?> get dataStream$ => _dataStream.stream;
+
+  void close() {
+    _dataStream.close();
+  }
+
+  void addEntity(T entity) {
+    final List<T> updatedData = [...?_dataStream.value];
+    final int entityIndex = updatedData.indexWhere(
+      (T existingEntity) => existingEntity.id == entity.id,
+    );
+    if (entityIndex >= 0) {
+      final int entityIndex = updatedData.indexWhere(
+        (T existingEntity) => existingEntity.id == entity.id,
+      );
+      updatedData[entityIndex] = entity;
+    } else {
+      updatedData.add(entity);
+    }
+    _dataStream.add(updatedData);
+  }
+}
