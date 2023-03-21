cdcf4f4332de86f606249554d1721889468eb3b8
Wojtas399 <wojtekp26@gmail.com>
2023-03-21 10:30:56 +0100

    Initial setup of home pages.

---
+++ lib/presentation/screen/current_week/ui/current_week_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CurrentWeekScreen extends StatelessWidget {
+  const CurrentWeekScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Current week screen'),
+    );
+  }
+}
