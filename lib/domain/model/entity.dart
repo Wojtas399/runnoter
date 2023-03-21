5155c7d76908b64d27b129a1e1930d220d3b2d36
Wojtas399 <wojtekp26@gmail.com>
2023-03-21 21:50:49 +0100

    User domain model.

---
+++ lib/domain/model/entity.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Entity extends Equatable {
+  final String id;
+
+  const Entity({
+    required this.id,
+  });
+
+  @override
+  List<Object> get props => [
+        id,
+      ];
+}
