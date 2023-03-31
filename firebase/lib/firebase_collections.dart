9152b549de5af147bb2bbf9a57d6b9c6f68cc432
Wojtas399 <wojtekp26@gmail.com>
2023-03-31 17:54:09 +0200

    References to appearance and workout settings.

--- firebase/lib/firebase_collections.dart
+++ firebase/lib/firebase_collections.dart
@@ -13,3 +13,33 @@ CollectionReference<UserDto> getUsersRef() {
 DocumentReference<UserDto> getUserRef(String userId) {
   return getUsersRef().doc(userId);
 }
+
+DocumentReference<AppearanceSettingsDto> getAppearanceSettingsRef(
+  String userId,
+) {
+  return getUserRef(userId)
+      .collection('Settings')
+      .doc('AppearanceSettings')
+      .withConverter<AppearanceSettingsDto>(
+        fromFirestore: (snapshot, _) => AppearanceSettingsDto.fromJson(
+          userId,
+          snapshot.data(),
+        ),
+        toFirestore: (appearanceSettings, _) => appearanceSettings.toJson(),
+      );
+}
+
+DocumentReference<WorkoutSettingsDto> getWorkoutSettingsRef(
+  String userId,
+) {
+  return getUserRef(userId)
+      .collection('Settings')
+      .doc('WorkoutSettings')
+      .withConverter<WorkoutSettingsDto>(
+        fromFirestore: (snapshot, _) => WorkoutSettingsDto.fromJson(
+          userId,
+          snapshot.data(),
+        ),
+        toFirestore: (workoutSettingsDto, _) => workoutSettingsDto.toJson(),
+      );
+}
