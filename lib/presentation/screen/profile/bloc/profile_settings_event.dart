b748f69771296a0daeb77a40d8590e5df8b444a8
Wojtas399 <wojtekp26@gmail.com>
2023-04-02 21:36:23 +0200

    Profile settings bloc.

--- lib/presentation/screen/profile/bloc/profile_settings_event.dart
+++ lib/presentation/screen/profile/bloc/profile_settings_event.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
 
+import '../../../../domain/model/user.dart';
+
 abstract class ProfileSettingsEvent extends Equatable {
   const ProfileSettingsEvent();
 
@@ -9,4 +11,15 @@ abstract class ProfileSettingsEvent extends Equatable {
 
 class ProfileSettingsEventInitialize extends ProfileSettingsEvent {
   const ProfileSettingsEventInitialize();
+}
+
+class ProfileSettingsEventUserUpdated extends ProfileSettingsEvent {
+  final User? user;
+
+  const ProfileSettingsEventUserUpdated({
+    required this.user,
+  });
+
+  @override
+  List<Object> get props => [];
 }
