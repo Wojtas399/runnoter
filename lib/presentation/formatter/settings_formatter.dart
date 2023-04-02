d13fb6828f16ac74d340185dd41d64d34bafbc7e
Wojtas399 <wojtekp26@gmail.com>
2023-04-02 22:49:17 +0200

    Language UI formatter.

--- lib/presentation/formatter/settings_formatter.dart
+++ lib/presentation/formatter/settings_formatter.dart
@@ -1,15 +1,28 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../domain/model/settings.dart';
 
 extension ThemeModeFormatter on ThemeMode {
   String toUIFormat(BuildContext context) {
     switch (this) {
       case ThemeMode.system:
-        return AppLocalizations.of(context)!.theme_mode_system;
+        return AppLocalizations.of(context)!.settings_theme_mode_system;
       case ThemeMode.light:
-        return AppLocalizations.of(context)!.theme_mode_light;
+        return AppLocalizations.of(context)!.settings_theme_mode_light;
       case ThemeMode.dark:
-        return AppLocalizations.of(context)!.theme_mode_dark;
+        return AppLocalizations.of(context)!.settings_theme_mode_dark;
+    }
+  }
+}
+
+extension LanguageFormatter on Language {
+  String toUIFormat(BuildContext context) {
+    switch (this) {
+      case Language.polish:
+        return AppLocalizations.of(context)!.settings_language_polish;
+      case Language.english:
+        return AppLocalizations.of(context)!.settings_language_english;
     }
   }
 }
