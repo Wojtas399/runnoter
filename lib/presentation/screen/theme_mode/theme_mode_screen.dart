f1b1b5d798c9709e409a5dee24c848df2eec9e52
Wojtas399 <wojtekp26@gmail.com>
2023-04-05 07:18:01 +0200

    Provided cubit on theme mode screen.

--- lib/presentation/screen/theme_mode/theme_mode_screen.dart
+++ lib/presentation/screen/theme_mode/theme_mode_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../../domain/model/settings.dart' as settings;
+import '../../../domain/repository/user_repository.dart';
+import '../../../domain/service/auth_service.dart';
 import '../../service/navigator_service.dart';
+import 'theme_mode_cubit.dart';
 
 class ThemeModeScreen extends StatelessWidget {
   const ThemeModeScreen({
@@ -11,58 +15,95 @@ class ThemeModeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const settings.ThemeMode selectedThemeMode = settings.ThemeMode.light;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          AppLocalizations.of(context)!.profile_screen_theme_mode_label,
+    return _CubitProvider(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            AppLocalizations.of(context)!.profile_screen_theme_mode_label,
+          ),
+          leading: IconButton(
+            onPressed: () {
+              navigateBack(context: context);
+            },
+            icon: const Icon(Icons.close),
+          ),
         ),
-        leading: IconButton(
-          onPressed: () {
-            navigateBack(context: context);
-          },
-          icon: const Icon(Icons.close),
-        ),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                RadioListTile<settings.ThemeMode>(
-                  title: Text(
-                    AppLocalizations.of(context)!.settings_theme_mode_light,
-                  ),
-                  value: settings.ThemeMode.light,
-                  groupValue: selectedThemeMode,
-                  onChanged: _onThemeModeSelected,
-                ),
-                RadioListTile<settings.ThemeMode>(
-                  title: Text(
-                    AppLocalizations.of(context)!.settings_theme_mode_dark,
-                  ),
-                  value: settings.ThemeMode.dark,
-                  groupValue: selectedThemeMode,
-                  onChanged: _onThemeModeSelected,
-                ),
-                RadioListTile<settings.ThemeMode>(
-                  title: Text(
-                    AppLocalizations.of(context)!.settings_theme_mode_system,
-                  ),
-                  value: settings.ThemeMode.system,
-                  groupValue: selectedThemeMode,
-                  onChanged: _onThemeModeSelected,
-                ),
-              ],
-            ),
-          ],
+        body: const SafeArea(
+          child: _OptionsToSelect(),
         ),
       ),
     );
   }
+}
 
-  void _onThemeModeSelected(settings.ThemeMode? themeMode) {
+class _CubitProvider extends StatelessWidget {
+  final Widget child;
+
+  const _CubitProvider({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) => ThemeModeCubit(
+        authService: context.read<AuthService>(),
+        userRepository: context.read<UserRepository>(),
+      )..initialize(),
+      child: child,
+    );
+  }
+}
+
+class _OptionsToSelect extends StatelessWidget {
+  const _OptionsToSelect();
+
+  @override
+  Widget build(BuildContext context) {
+    final settings.ThemeMode? selectedThemeMode = context.select(
+      (ThemeModeCubit cubit) => cubit.state,
+    );
+
+    return Column(
+      children: [
+        RadioListTile<settings.ThemeMode>(
+          title: Text(
+            AppLocalizations.of(context)!.settings_theme_mode_light,
+          ),
+          value: settings.ThemeMode.light,
+          groupValue: selectedThemeMode,
+          onChanged: (settings.ThemeMode? themeMode) {
+            _onThemeModeSelected(context, themeMode);
+          },
+        ),
+        RadioListTile<settings.ThemeMode>(
+          title: Text(
+            AppLocalizations.of(context)!.settings_theme_mode_dark,
+          ),
+          value: settings.ThemeMode.dark,
+          groupValue: selectedThemeMode,
+          onChanged: (settings.ThemeMode? themeMode) {
+            _onThemeModeSelected(context, themeMode);
+          },
+        ),
+        RadioListTile<settings.ThemeMode>(
+          title: Text(
+            AppLocalizations.of(context)!.settings_theme_mode_system,
+          ),
+          value: settings.ThemeMode.system,
+          groupValue: selectedThemeMode,
+          onChanged: (settings.ThemeMode? themeMode) {
+            _onThemeModeSelected(context, themeMode);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _onThemeModeSelected(
+    BuildContext context,
+    settings.ThemeMode? themeMode,
+  ) {
     //TODO
   }
 }
