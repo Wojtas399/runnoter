dcf6e9e5bef821ff79caeeb373db33837d03f751
Wojtas399 <wojtekp26@gmail.com>
2023-04-08 20:41:48 +0200

    Current week cubit provider.

---
+++ lib/presentation/screen/current_week/ui/current_week_content.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CurrentWeekContent extends StatelessWidget {
+  const CurrentWeekContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Current week screen'),
+    );
+  }
+}
