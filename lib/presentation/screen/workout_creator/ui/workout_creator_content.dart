441549d9a911043d3c1f13dda8b06cebfcb2657e
Wojtas399 <wojtekp26@gmail.com>
2023-04-17 22:06:50 +0200

    Workout name text field.

--- lib/presentation/screen/workout_creator/ui/workout_creator_content.dart
+++ lib/presentation/screen/workout_creator/ui/workout_creator_content.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../component/scrollable_content_component.dart';
+import '../../../component/text_field_component.dart';
+import '../../../service/utils.dart';
+import '../bloc/workout_creator_bloc.dart';
+import '../bloc/workout_creator_event.dart';
 
 class WorkoutCreatorContent extends StatelessWidget {
   const WorkoutCreatorContent({
@@ -9,8 +17,51 @@ class WorkoutCreatorContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Workout creator'),
+        title: Text(
+          AppLocalizations.of(context)!.workout_creator_screen_title,
+        ),
+      ),
+      body: SafeArea(
+        child: ScrollableContent(
+          child: GestureDetector(
+            onTap: () {
+              unfocusInputs();
+            },
+            child: Container(
+              color: Colors.transparent,
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                children: const [
+                  _WorkoutName(),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
+
+class _WorkoutName extends StatelessWidget {
+  const _WorkoutName();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldComponent(
+      label: AppLocalizations.of(context)!.workout_creator_screen_workout_name,
+      isRequired: true,
+      onChanged: (String? workoutName) {
+        _onChanged(context, workoutName);
+      },
+    );
+  }
+
+  void _onChanged(BuildContext context, String? workoutName) {
+    context.read<WorkoutCreatorBloc>().add(
+          WorkoutCreatorEventWorkoutNameChanged(
+            workoutName: workoutName,
+          ),
+        );
   }
 }
