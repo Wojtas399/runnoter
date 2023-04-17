9e909d3d7c51aec6ad85accb93ef80021deafffc
Wojtas399 <wojtekp26@gmail.com>
2023-04-17 21:30:38 +0200

    Workout creator bloc provider.

---
+++ lib/presentation/screen/workout_creator/ui/workout_creator_content.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WorkoutCreatorContent extends StatelessWidget {
+  const WorkoutCreatorContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Workout creator'),
+      ),
+    );
+  }
+}
--- lib/presentation/screen/workout_creator/ui/workout_creator_screen.dart
+++ lib/presentation/screen/workout_creator/ui/workout_creator_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/workout_creator_bloc.dart';
+import '../bloc/workout_creator_event.dart';
+import 'workout_creator_content.dart';
 
 class WorkoutCreatorScreen extends StatelessWidget {
   final DateTime date;
@@ -10,10 +15,30 @@ class WorkoutCreatorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Workout creator'),
-      ),
+    return _BlocProvider(
+      date: date,
+      child: const WorkoutCreatorContent(),
+    );
+  }
+}
+
+class _BlocProvider extends StatelessWidget {
+  final DateTime date;
+  final Widget child;
+
+  const _BlocProvider({
+    required this.date,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => WorkoutCreatorBloc()
+        ..add(
+          WorkoutCreatorEventInitialize(date: date),
+        ),
+      child: child,
     );
   }
 }
