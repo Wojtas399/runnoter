495e8092ca14525ffa26b4499cb490efd41c8a68
Wojtas399 <wojtekp26@gmail.com>
2023-04-19 21:39:46 +0200

    Series state of workout stage creator.

---
+++ lib/presentation/screen/workout_stage_creator/bloc/workout_stage_creator_series_stage_state.dart
@@ -0,0 +1,59 @@
+import 'workout_stage_creator_state.dart';
+
+class WorkoutStageCreatorSeriesStageState extends WorkoutStageCreatorState {
+  final int? amountOfSeries;
+  final int? seriesDistanceInMeters;
+  final int? breakWalkingDistanceInMeters;
+  final int? breakJoggingDistanceInMeters;
+
+  const WorkoutStageCreatorSeriesStageState({
+    required this.amountOfSeries,
+    required this.seriesDistanceInMeters,
+    required this.breakWalkingDistanceInMeters,
+    required this.breakJoggingDistanceInMeters,
+  });
+
+  @override
+  List<Object?> get props => [
+        amountOfSeries,
+        seriesDistanceInMeters,
+        breakWalkingDistanceInMeters,
+        breakJoggingDistanceInMeters,
+      ];
+
+  @override
+  bool get areDataCorrect =>
+      _isAmountOfSeriesCorrect &&
+      _isSeriesDistanceInMetersCorrect &&
+      (_isBreakWalkingDistanceInMetersCorrect ||
+          _isBreakJoggingDistanceInMetersCorrect);
+
+  bool get _isAmountOfSeriesCorrect =>
+      amountOfSeries != null && amountOfSeries! > 0;
+
+  bool get _isSeriesDistanceInMetersCorrect =>
+      seriesDistanceInMeters != null && seriesDistanceInMeters! > 0;
+
+  bool get _isBreakWalkingDistanceInMetersCorrect =>
+      breakWalkingDistanceInMeters != null && breakWalkingDistanceInMeters! > 0;
+
+  bool get _isBreakJoggingDistanceInMetersCorrect =>
+      breakJoggingDistanceInMeters != null && breakJoggingDistanceInMeters! > 0;
+
+  @override
+  WorkoutStageCreatorSeriesStageState copyWith({
+    int? amountOfSeries,
+    int? seriesDistanceInMeters,
+    int? breakWalkingDistanceInMeters,
+    int? breakJoggingDistanceInMeters,
+  }) =>
+      WorkoutStageCreatorSeriesStageState(
+        amountOfSeries: amountOfSeries ?? this.amountOfSeries,
+        seriesDistanceInMeters:
+            seriesDistanceInMeters ?? this.seriesDistanceInMeters,
+        breakWalkingDistanceInMeters:
+            breakWalkingDistanceInMeters ?? this.breakWalkingDistanceInMeters,
+        breakJoggingDistanceInMeters:
+            breakJoggingDistanceInMeters ?? this.breakJoggingDistanceInMeters,
+      );
+}
---
+++ test/presentation/bloc/workout_stage_creator/workout_stage_creator_series_stage_state_test.dart
@@ -0,0 +1,224 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:runnoter/presentation/screen/workout_stage_creator/bloc/workout_stage_creator_series_stage_state.dart';
+
+void main() {
+  late WorkoutStageCreatorSeriesStageState state;
+
+  WorkoutStageCreatorSeriesStageState createState({
+    int? amountOfSeries,
+    int? seriesDistanceInMeters,
+    int? breakWalkingDistanceInMeters,
+    int? breakJoggingDistanceInMeters,
+  }) =>
+      WorkoutStageCreatorSeriesStageState(
+        amountOfSeries: amountOfSeries,
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakWalkingDistanceInMeters: breakWalkingDistanceInMeters,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+  test(
+    'are data correct, '
+    'amount of series is higher than 0, '
+    'series distance in meters is higher than 0, '
+    'break walking distance is higher than 0, '
+    'should be true',
+    () {
+      const int amountOfSeries = 10;
+      const int seriesDistanceInMeters = 100;
+      const int breakWalkingDistanceInMeters = 100;
+
+      state = createState(
+        amountOfSeries: amountOfSeries,
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakWalkingDistanceInMeters: breakWalkingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, true);
+    },
+  );
+
+  test(
+    'are data correct, '
+    'amount of series is higher than 0, '
+    'series distance in meters is higher than 0, '
+    'break jogging distance is higher than 0, '
+    'should be true',
+    () {
+      const int amountOfSeries = 10;
+      const int seriesDistanceInMeters = 100;
+      const int breakJoggingDistanceInMeters = 100;
+
+      state = createState(
+        amountOfSeries: amountOfSeries,
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, true);
+    },
+  );
+
+  test(
+    'are data correct, '
+    'amount of series is null, '
+    'should be false',
+    () {
+      const int seriesDistanceInMeters = 100;
+      const int breakJoggingDistanceInMeters = 100;
+
+      state = createState(
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, false);
+    },
+  );
+
+  test(
+    'are data correct, '
+    'amount of series is 0, '
+    'should be false',
+    () {
+      const int amountOfSeries = 0;
+      const int seriesDistanceInMeters = 100;
+      const int breakJoggingDistanceInMeters = 100;
+
+      state = createState(
+        amountOfSeries: amountOfSeries,
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, false);
+    },
+  );
+
+  test(
+    'are data correct, '
+    'series distance in meters is null, '
+    'should be false',
+    () {
+      const int amountOfSeries = 10;
+      const int breakJoggingDistanceInMeters = 100;
+
+      state = createState(
+        amountOfSeries: amountOfSeries,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, false);
+    },
+  );
+
+  test(
+    'are data correct, '
+    'series distance in meters is 0, '
+    'should be false',
+    () {
+      const int amountOfSeries = 10;
+      const seriesDistanceInMeters = 0;
+      const int breakJoggingDistanceInMeters = 100;
+
+      state = createState(
+        amountOfSeries: amountOfSeries,
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, false);
+    },
+  );
+
+  test(
+    'are data correct, '
+    'break walking and jogging distances are 0, '
+    'should be false',
+    () {
+      const int amountOfSeries = 10;
+      const int seriesDistanceInMeters = 100;
+      const int breakWalkingDistanceInMeters = 0;
+      const int breakJoggingDistanceInMeters = 0;
+
+      state = createState(
+        amountOfSeries: amountOfSeries,
+        seriesDistanceInMeters: seriesDistanceInMeters,
+        breakWalkingDistanceInMeters: breakWalkingDistanceInMeters,
+        breakJoggingDistanceInMeters: breakJoggingDistanceInMeters,
+      );
+
+      expect(state.areDataCorrect, false);
+    },
+  );
+
+  test(
+    'copy with amount of series',
+    () {
+      const int expectedAmountOfSeries = 10;
+
+      state = state.copyWith(amountOfSeries: expectedAmountOfSeries);
+      final state2 = state.copyWith();
+
+      expect(state.amountOfSeries, expectedAmountOfSeries);
+      expect(state2.amountOfSeries, expectedAmountOfSeries);
+    },
+  );
+
+  test(
+    'copy with series distance in meters',
+    () {
+      const int expectedSeriesDistanceInMeters = 100;
+
+      state = state.copyWith(
+        seriesDistanceInMeters: expectedSeriesDistanceInMeters,
+      );
+      final state2 = state.copyWith();
+
+      expect(state.seriesDistanceInMeters, expectedSeriesDistanceInMeters);
+      expect(state2.seriesDistanceInMeters, expectedSeriesDistanceInMeters);
+    },
+  );
+
+  test(
+    'copy with break walking distance in meters',
+    () {
+      const int expectedBreakWalkingDistanceInMeters = 20;
+
+      state = state.copyWith(
+        breakWalkingDistanceInMeters: expectedBreakWalkingDistanceInMeters,
+      );
+      final state2 = state.copyWith();
+
+      expect(
+        state.breakWalkingDistanceInMeters,
+        expectedBreakWalkingDistanceInMeters,
+      );
+      expect(
+        state2.breakWalkingDistanceInMeters,
+        expectedBreakWalkingDistanceInMeters,
+      );
+    },
+  );
+
+  test(
+    'copy with break jogging distance in meters',
+    () {
+      const int expectedBreakJoggingDistanceInMeters = 80;
+
+      state = state.copyWith(
+        breakJoggingDistanceInMeters: expectedBreakJoggingDistanceInMeters,
+      );
+      final state2 = state.copyWith();
+
+      expect(
+        state.breakJoggingDistanceInMeters,
+        expectedBreakJoggingDistanceInMeters,
+      );
+      expect(
+        state2.breakJoggingDistanceInMeters,
+        expectedBreakJoggingDistanceInMeters,
+      );
+    },
+  );
+}
