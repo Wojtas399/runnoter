45885e155fbddca67ad0b974a67a214361624c56
Wojtas399 <wojtekp26@gmail.com>
2023-04-21 09:43:07 +0200

    Events to edit forms.

--- lib/presentation/screen/workout_stage_creator/bloc/workout_stage_creator_event.dart
+++ lib/presentation/screen/workout_stage_creator/bloc/workout_stage_creator_event.dart
@@ -12,3 +12,56 @@ class WorkoutStageCreatorEventStageTypeChanged
     required this.stage,
   });
 }
+
+class WorkoutStageCreatorEventDistanceChanged extends WorkoutStageCreatorEvent {
+  final double distanceInKm;
+
+  const WorkoutStageCreatorEventDistanceChanged({
+    required this.distanceInKm,
+  });
+}
+
+class WorkoutStageCreatorEventMaxHeartRateChanged
+    extends WorkoutStageCreatorEvent {
+  final int maxHeartRate;
+
+  const WorkoutStageCreatorEventMaxHeartRateChanged({
+    required this.maxHeartRate,
+  });
+}
+
+class WorkoutStageCreatorEventAmountOfSeriesChanged
+    extends WorkoutStageCreatorEvent {
+  final int amountOfSeries;
+
+  const WorkoutStageCreatorEventAmountOfSeriesChanged({
+    required this.amountOfSeries,
+  });
+}
+
+class WorkoutStageCreatorEventSeriesDistanceChanged
+    extends WorkoutStageCreatorEvent {
+  final int seriesDistanceInMeters;
+
+  const WorkoutStageCreatorEventSeriesDistanceChanged({
+    required this.seriesDistanceInMeters,
+  });
+}
+
+class WorkoutStageCreatorEventWalkingDistanceChanged
+    extends WorkoutStageCreatorEvent {
+  final int walkingDistanceInMeters;
+
+  const WorkoutStageCreatorEventWalkingDistanceChanged({
+    required this.walkingDistanceInMeters,
+  });
+}
+
+class WorkoutStageCreatorEventJoggingDistanceChanged
+    extends WorkoutStageCreatorEvent {
+  final int joggingDistanceInMeters;
+
+  const WorkoutStageCreatorEventJoggingDistanceChanged({
+    required this.joggingDistanceInMeters,
+  });
+}
