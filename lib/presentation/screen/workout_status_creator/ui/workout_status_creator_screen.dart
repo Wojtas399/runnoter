a4f8a21959baf3f28202976fe5ed9cc748d00a16
Wojtas399 <wojtekp26@gmail.com>
2023-05-02 17:53:12 +0200

    Provider for workout status creator bloc.

--- lib/presentation/screen/workout_status_creator/ui/workout_status_creator_screen.dart
+++ lib/presentation/screen/workout_status_creator/ui/workout_status_creator_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../../domain/model/workout_status.dart';
@@ -10,6 +11,7 @@ import '../../../formatter/decimal_text_input_formatter.dart';
 import '../../../formatter/mood_rate_formatter.dart';
 import '../../../formatter/workout_status_formatter.dart';
 import '../../../service/utils.dart';
+import '../bloc/workout_status_creator_bloc.dart';
 
 part 'workout_status_creator_average_pace.dart';
 part 'workout_status_creator_content.dart';
@@ -25,6 +27,24 @@ class WorkoutStatusCreatorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const _Content();
+    return const _BlocProvider(
+      child: _Content(),
+    );
+  }
+}
+
+class _BlocProvider extends StatelessWidget {
+  final Widget child;
+
+  const _BlocProvider({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => WorkoutStatusCreatorBloc(),
+      child: child,
+    );
   }
 }
