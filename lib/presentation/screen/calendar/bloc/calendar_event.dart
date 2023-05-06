0fa3356d9226c2f43eb492bd3de2109ff6370bed
Wojtas399 <wojtekp26@gmail.com>
2023-05-06 16:00:35 +0200

    Calendar events.

---
+++ lib/presentation/screen/calendar/bloc/calendar_event.dart
@@ -0,0 +1,17 @@
+abstract class CalendarEvent {
+  const CalendarEvent();
+}
+
+class CalendarEventInitialize extends CalendarEvent {
+  const CalendarEventInitialize();
+}
+
+class CalendarEventMonthChanged extends CalendarEvent {
+  final int month;
+  final int year;
+
+  const CalendarEventMonthChanged({
+    required this.month,
+    required this.year,
+  });
+}
