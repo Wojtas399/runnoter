cc3cb0359c270bc09f587b5d26487233b967b646
Wojtas399 <wojtekp26@gmail.com>
2023-05-09 22:36:45 +0200

    Health bloc provider.

---
+++ lib/presentation/screen/health/ui/health_content.dart
@@ -0,0 +1,12 @@
+part of 'health_screen.dart';
+
+class _Content extends StatelessWidget {
+  const _Content();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Health screen'),
+    );
+  }
+}
--- lib/presentation/screen/health/ui/health_screen.dart
+++ lib/presentation/screen/health/ui/health_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../common/date_service.dart';
+import '../../../../domain/repository/morning_measurement_repository.dart';
+import '../../../../domain/service/auth_service.dart';
+import '../bloc/health_bloc.dart';
+
+part 'health_content.dart';
 
 class HealthScreen extends StatelessWidget {
   const HealthScreen({
@@ -7,8 +15,29 @@ class HealthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Health screen'),
+    return const _BlocProvider(
+      child: _Content(),
+    );
+  }
+}
+
+class _BlocProvider extends StatelessWidget {
+  final Widget child;
+
+  const _BlocProvider({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) => HealthBloc(
+        dateService: DateService(),
+        authService: context.read<AuthService>(),
+        morningMeasurementRepository:
+            context.read<MorningMeasurementRepository>(),
+      ),
+      child: child,
     );
   }
 }
