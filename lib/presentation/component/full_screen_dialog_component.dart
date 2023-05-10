f9bf72acf3970dc3ad3c6fca6ab4169b239503e1
Wojtas399 <wojtekp26@gmail.com>
2023-05-10 09:56:42 +0200

    Full screen dialog component.

---
+++ lib/presentation/component/full_screen_dialog_component.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../service/navigator_service.dart';
+
+class FullScreenDialog extends StatelessWidget {
+  final String title;
+  final String? submitButtonLabel;
+  final bool isSubmitButtonDisabled;
+  final Widget body;
+  final VoidCallback? onSubmitButtonPressed;
+
+  const FullScreenDialog({
+    super.key,
+    required this.title,
+    this.submitButtonLabel,
+    this.isSubmitButtonDisabled = false,
+    required this.body,
+    this.onSubmitButtonPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        leading: IconButton(
+          onPressed: () {
+            navigateBack(context: context);
+          },
+          icon: const Icon(Icons.close),
+        ),
+        actions: [
+          TextButton(
+            onPressed: isSubmitButtonDisabled ? null : onSubmitButtonPressed,
+            child: Text(
+              submitButtonLabel ?? Str.of(context).save,
+            ),
+          ),
+          const SizedBox(width: 16),
+        ],
+      ),
+      body: body,
+    );
+  }
+}
