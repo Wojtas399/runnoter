65f90152fcc5e64d929d03a75ab5a8dbf3696067
Wojtas399 <wojtekp26@gmail.com>
2023-05-16 08:10:59 +0200

    Health measurements events.

---
+++ lib/presentation/screen/health_measurements/bloc/health_measurements_event.dart
@@ -0,0 +1,15 @@
+abstract class HealthMeasurementsEvent {
+  const HealthMeasurementsEvent();
+}
+
+class HealthMeasurementsEventInitialize extends HealthMeasurementsEvent {
+  const HealthMeasurementsEventInitialize();
+}
+
+class HealthMeasurementsEventDeleteMeasurement extends HealthMeasurementsEvent {
+  final DateTime date;
+
+  const HealthMeasurementsEventDeleteMeasurement({
+    required this.date,
+  });
+}
