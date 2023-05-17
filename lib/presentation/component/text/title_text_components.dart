55df577b8a80f4762d145aff91061fe18c65d0a9
Wojtas399 <wojtekp26@gmail.com>
2023-05-17 21:09:04 +0200

    Title text components.

---
+++ lib/presentation/component/text/title_text_components.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TitleLarge extends StatelessWidget {
+  final String data;
+
+  const TitleLarge(
+    this.data, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: Theme.of(context).textTheme.titleLarge,
+    );
+  }
+}
+
+class TitleMedium extends StatelessWidget {
+  final String data;
+
+  const TitleMedium(
+    this.data, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: Theme.of(context).textTheme.titleMedium,
+    );
+  }
+}
