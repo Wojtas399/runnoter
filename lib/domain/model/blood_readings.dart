a9ce199d4dfa8b462ad9ef285ec4ce33473171cb
Wojtas399 <wojtekp26@gmail.com>
2023-05-24 12:47:35 +0200

    Blood readings model.

---
+++ lib/domain/model/blood_readings.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+import 'blood_test_parameter.dart';
+import 'entity.dart';
+
+class BloodReadings extends Entity {
+  final String userId;
+  final DateTime date;
+  final List<BloodParameterReading> readings;
+
+  const BloodReadings({
+    required super.id,
+    required this.userId,
+    required this.date,
+    required this.readings,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        userId,
+        date,
+        readings,
+      ];
+}
+
+class BloodParameterReading extends Equatable {
+  final BloodTestParameter parameter;
+  final double reading;
+
+  const BloodParameterReading({
+    required this.parameter,
+    required this.reading,
+  });
+
+  @override
+  List<Object?> get props => [
+        parameter,
+        reading,
+      ];
+}
