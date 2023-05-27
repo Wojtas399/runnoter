def8ffed3cfbfa798ed853412c6bb2aa10cb83e1
Wojtas399 <wojtekp26@gmail.com>
2023-05-27 22:32:13 +0200

    Mock of blood reading repository.

---
+++ test/mock/domain/mock_blood_reading_repository.dart
@@ -0,0 +1,26 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:runnoter/domain/model/blood_reading.dart';
+import 'package:runnoter/domain/repository/blood_reading_repository.dart';
+
+class MockBloodReadingRepository extends Mock
+    implements BloodReadingRepository {
+  void mockGetAllReadings({
+    List<BloodReading>? readings,
+  }) {
+    when(
+      () => getAllReadings(
+        userId: any(named: 'userId'),
+      ),
+    ).thenAnswer((invocation) => Stream.value(readings));
+  }
+
+  void mockAddNewReading() {
+    when(
+      () => addNewReading(
+        userId: any(named: 'userId'),
+        date: any(named: 'date'),
+        parameters: any(named: 'parameters'),
+      ),
+    ).thenAnswer((invocation) => Future.value());
+  }
+}
