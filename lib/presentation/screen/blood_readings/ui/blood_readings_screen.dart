7157fed49baba4c36953b811d6c206ceaa6c17b0
Wojtas399 <wojtekp26@gmail.com>
2023-05-28 20:38:00 +0200

    Blood readings cubit provider.

--- lib/presentation/screen/blood_readings/ui/blood_readings_screen.dart
+++ lib/presentation/screen/blood_readings/ui/blood_readings_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../../../domain/repository/blood_reading_repository.dart';
+import '../../../../domain/service/auth_service.dart';
 import '../../../component/big_button_component.dart';
 import '../../../config/navigation/routes.dart';
 import '../../../service/navigator_service.dart';
+import '../blood_readings_cubit.dart';
 
 class BloodReadingsScreen extends StatelessWidget {
   const BloodReadingsScreen({
@@ -12,17 +16,19 @@ class BloodReadingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        children: [
-          BigButton(
-            label: Str.of(context).bloodAddBloodTest,
-            onPressed: () {
-              _onPressed(context);
-            },
-          ),
-        ],
+    return _CubitProvider(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            BigButton(
+              label: Str.of(context).bloodAddBloodTest,
+              onPressed: () {
+                _onPressed(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -31,6 +37,25 @@ class BloodReadingsScreen extends StatelessWidget {
     navigateTo(
       context: context,
       route: const BloodTestCreatorRoute(),
+    );
+  }
+}
+
+class _CubitProvider extends StatelessWidget {
+  final Widget child;
+
+  const _CubitProvider({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => BloodReadingsCubit(
+        authService: context.read<AuthService>(),
+        bloodReadingRepository: context.read<BloodReadingRepository>(),
+      )..initialize(),
+      child: child,
     );
   }
 }
