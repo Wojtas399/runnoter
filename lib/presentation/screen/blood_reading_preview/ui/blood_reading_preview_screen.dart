db8d3a426b7d8c486407f51f18c30c7d621d5e70
Wojtas399 <wojtekp26@gmail.com>
2023-05-31 08:27:25 +0200

    Blood reading preview bloc provider.

--- lib/presentation/screen/blood_reading_preview/ui/blood_reading_preview_screen.dart
+++ lib/presentation/screen/blood_reading_preview/ui/blood_reading_preview_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../domain/repository/blood_reading_repository.dart';
+import '../../../../domain/service/auth_service.dart';
+import '../bloc/blood_reading_preview_bloc.dart';
 
 class BloodReadingPreviewScreen extends StatelessWidget {
   final String bloodReadingId;
@@ -10,10 +15,38 @@ class BloodReadingPreviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Blood reading preview'),
+    return _BlocProvider(
+      bloodReadingId: bloodReadingId,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Blood reading preview'),
+        ),
       ),
+    );
+  }
+}
+
+class _BlocProvider extends StatelessWidget {
+  final String bloodReadingId;
+  final Widget child;
+
+  const _BlocProvider({
+    required this.bloodReadingId,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) => BloodReadingPreviewBloc(
+        authService: context.read<AuthService>(),
+        bloodReadingRepository: context.read<BloodReadingRepository>(),
+      )..add(
+          BloodReadingPreviewEventInitialize(
+            bloodReadingId: bloodReadingId,
+          ),
+        ),
+      child: child,
     );
   }
 }
