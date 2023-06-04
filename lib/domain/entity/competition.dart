d2893e149b103072b9fdfce1495e241787b64f77
Wojtas399 <wojtekp26@gmail.com>
2023-06-04 23:34:21 +0200

    Competition entity.

---
+++ lib/domain/entity/competition.dart
@@ -0,0 +1,56 @@
+import 'package:equatable/equatable.dart';
+
+import 'entity.dart';
+import 'workout_status.dart';
+
+class Competition extends Entity {
+  final String userId;
+  final String name;
+  final DateTime date;
+  final String city;
+  final double distance;
+  final Time expectedTime;
+  final WorkoutStatus status;
+
+  const Competition({
+    required super.id,
+    required this.userId,
+    required this.name,
+    required this.date,
+    required this.city,
+    required this.distance,
+    required this.expectedTime,
+    required this.status,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        userId,
+        name,
+        date,
+        city,
+        distance,
+        expectedTime,
+        status,
+      ];
+}
+
+class Time extends Equatable {
+  final int hour;
+  final int minute;
+  final int second;
+
+  const Time({
+    required this.hour,
+    required this.minute,
+    required this.second,
+  });
+
+  @override
+  List<Object?> get props => [
+        hour,
+        minute,
+        second,
+      ];
+}
