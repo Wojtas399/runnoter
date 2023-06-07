b60277b06648b52f09e7f80e2145ad85350cbd8f
Wojtas399 <wojtekp26@gmail.com>
2023-06-07 13:59:29 +0200

    Competitions cubit provider.

--- lib/presentation/screen/competitions/competitions_screen.dart
+++ lib/presentation/screen/competitions/competitions_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../domain/bloc/competitions/competitions_cubit.dart';
+import '../../../domain/repository/competition_repository.dart';
+import '../../../domain/service/auth_service.dart';
 import '../../component/big_button_component.dart';
 import '../../config/navigation/routes.dart';
 import '../../service/navigator_service.dart';
@@ -11,21 +15,42 @@ class CompetitionsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        children: [
-          BigButton(
-            label: 'Nowe zawody',
-            onPressed: () {
-              navigateTo(
-                context: context,
-                route: const CompetitionCreatorRoute(),
-              );
-            },
-          ),
-        ],
+    return _CubitProvider(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            BigButton(
+              label: 'Nowe zawody',
+              onPressed: () {
+                navigateTo(
+                  context: context,
+                  route: const CompetitionCreatorRoute(),
+                );
+              },
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _CubitProvider extends StatelessWidget {
+  final Widget child;
+
+  const _CubitProvider({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (BuildContext context) => CompetitionsCubit(
+        authService: context.read<AuthService>(),
+        competitionRepository: context.read<CompetitionRepository>(),
+      )..initialize(),
+      child: child,
     );
   }
 }
