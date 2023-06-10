4bb99f1f772cb4d08a15a8780e35984bfe70e256
Wojtas399 <wojtekp26@gmail.com>
2023-06-10 10:12:00 +0200

    Formatting workout total distance in day preview screen to match selected distance unit.

--- lib/presentation/formatter/list_of_workout_stages_formatter.dart
+++ lib/presentation/formatter/list_of_workout_stages_formatter.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/entity/workout_stage.dart';
+import '../../domain/service/distance_unit_service.dart';
 import '../service/workout_stage_service.dart';
+import 'distance_unit_formatter.dart';
 import 'workout_stage_formatter.dart';
 
 extension ListOfWorkoutStagesFormatter on List<WorkoutStage> {
   String toTotalDistanceDescription(BuildContext context) {
+    final distanceUnitService = context.read<DistanceUnitService>();
+    final String distanceUnit = distanceUnitService.state.toUIShortFormat();
     double totalDistance = 0;
-    List<String> stageDistanceDescriptions = [];
+    List<String> stageDescriptions = [];
     for (final stage in this) {
-      final double stageDistance = calculateDistanceOfWorkoutStage(stage);
-      totalDistance += stageDistance;
-      if (stage is DistanceWorkoutStage) {
-        stageDistanceDescriptions.add(
-          '${stage.toTypeName(context)} ${stage.distanceInKilometers} km',
-        );
-      } else if (stage is SeriesWorkoutStage) {
-        stageDistanceDescriptions.add(
-          '${stage.toTypeName(context)} $stageDistance km',
-        );
-      }
+      final double stageDistanceInKm = calculateDistanceOfWorkoutStage(stage);
+      final double convertedStageDistance = distanceUnitService.convertDistance(
+        stageDistanceInKm,
+      );
+      totalDistance += convertedStageDistance;
+      stageDescriptions.add(
+        '${stage.toTypeName(context)} $convertedStageDistance$distanceUnit',
+      );
     }
-    return '$totalDistance km (${stageDistanceDescriptions.join(' + ')})';
+    return '$totalDistance$distanceUnit (${stageDescriptions.join(' + ')})';
   }
 }
