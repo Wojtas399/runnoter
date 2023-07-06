8da86ec7c6aa96b9de37993630f3b9df65f1afc3
Wojtas399 <wojtekp26@gmail.com>
2023-07-06 12:09:23 +0200

    Made sign in screen responsive.

--- lib/presentation/screen/sign_in/sign_in_screen.dart
+++ lib/presentation/screen/sign_in/sign_in_screen.dart
@@ -7,6 +7,7 @@ import '../../../domain/service/auth_service.dart';
 import '../../component/big_button_component.dart';
 import '../../component/bloc_with_status_listener_component.dart';
 import '../../component/password_text_field_component.dart';
+import '../../component/scrollable_content_component.dart';
 import '../../component/text/headline_text_components.dart';
 import '../../component/text_field_component.dart';
 import '../../config/navigation/routes.dart';
--- lib/presentation/screen/sign_in/sign_in_screen_content.dart
+++ lib/presentation/screen/sign_in/sign_in_screen_content.dart
@@ -5,27 +5,34 @@ class _Content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(),
       body: SafeArea(
-        child: SingleChildScrollView(
+        child: ScrollableContent(
           child: GestureDetector(
             onTap: unfocusInputs,
-            child: Container(
-              color: Colors.transparent,
-              padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
-              child: const Column(
-                children: [
-                  _Logo(),
-                  SizedBox(height: 24),
-                  _FormHeader(),
-                  SizedBox(height: 32),
-                  _Form(),
-                  SizedBox(height: 32),
-                  _SubmitButton(),
-                  SizedBox(height: 16),
-                  _AlternativeOptions(),
-                ],
+            child: Center(
+              child: Card(
+                elevation: screenWidth < 500 ? 0 : null,
+                child: Container(
+                  color: Colors.transparent,
+                  padding: EdgeInsets.all(screenWidth < 500 ? 24 : 32),
+                  constraints: const BoxConstraints(maxWidth: 500),
+                  child: const Column(
+                    children: [
+                      _Logo(),
+                      SizedBox(height: 24),
+                      _FormHeader(),
+                      SizedBox(height: 32),
+                      _Form(),
+                      SizedBox(height: 32),
+                      _SubmitButton(),
+                      SizedBox(height: 16),
+                      _AlternativeOptions(),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
