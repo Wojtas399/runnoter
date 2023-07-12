9c9184b2f1623e1596ca067f1ac62365d30bb440
Wojtas399 <wojtekp26@gmail.com>
2023-07-12 08:38:48 +0200

    Disabled landscape orientation on mobile devices.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase/firebase.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeFirebaseApp();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
   runApp(
     const MyApp(),
   );
