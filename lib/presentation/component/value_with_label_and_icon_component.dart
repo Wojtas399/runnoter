fdf234647c9e05c5fdb7be856edd553c0d73c3e9
Wojtas399 <wojtekp26@gmail.com>
2023-07-15 12:57:10 +0200

    Repaired InkWell of component displaying value with label.

--- lib/presentation/component/value_with_label_and_icon_component.dart
+++ lib/presentation/component/value_with_label_and_icon_component.dart
@@ -20,44 +20,43 @@ class ValueWithLabelAndIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return OutlinedButton(
-      onPressed: onPressed,
-      style: OutlinedButton.styleFrom(
-        side: BorderSide(color: theme.colorScheme.background),
-        padding: const EdgeInsets.symmetric(horizontal: 4),
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: label != null ? 8 : 20,
-        ),
-        child: Row(
-          children: [
-            if (iconData != null)
-              Icon(
-                iconData,
-                color: color ?? theme.colorScheme.onSurfaceVariant,
-              ),
-            if (iconData != null) const SizedBox(width: 16),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                if (label != null)
-                  Text(
-                    label!,
-                    style: theme.textTheme.labelLarge?.copyWith(
-                      color: color ?? theme.colorScheme.onSurfaceVariant,
-                    ),
-                  ),
-                if (label != null) const SizedBox(height: 4),
-                Text(
-                  value,
-                  style: theme.textTheme.bodyLarge?.copyWith(color: color),
+    return Material(
+      child: InkWell(
+        borderRadius: BorderRadius.circular(100),
+        onTap: onPressed,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: label != null ? 8 : 20,
+          ),
+          child: Row(
+            children: [
+              if (iconData != null)
+                Icon(
+                  iconData,
+                  color: color ?? theme.colorScheme.onSurfaceVariant,
                 ),
-              ],
-            ),
-          ],
+              if (iconData != null) const SizedBox(width: 16),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (label != null)
+                    Text(
+                      label!,
+                      style: theme.textTheme.labelLarge?.copyWith(
+                        color: color ?? theme.colorScheme.onSurfaceVariant,
+                      ),
+                    ),
+                  if (label != null) const SizedBox(height: 4),
+                  Text(
+                    value,
+                    style: theme.textTheme.bodyLarge?.copyWith(color: color),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
