6547ad864d14713ecc307e96d5f637654d5dd945
Wojtas399 <wojtekp26@gmail.com>
2023-07-18 21:06:51 +0200

    Removed part directives from forgot password screen.

--- lib/presentation/screen/forgot_password/forgot_password_content.dart
+++ lib/presentation/screen/forgot_password/forgot_password_content.dart
@@ -1,9 +1,18 @@
-part of 'forgot_password_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../domain/bloc/forgot_password/forgot_password_bloc.dart';
+import '../../component/app_bar_with_logo.dart';
+import '../../component/big_button_component.dart';
+import '../../component/text/headline_text_components.dart';
+import '../../component/text_field_component.dart';
+import '../../config/body_sizes.dart';
+import '../../service/utils.dart';
 
 class ForgotPasswordContent extends StatelessWidget {
-  const ForgotPasswordContent({
-    super.key,
-  });
+  const ForgotPasswordContent({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -49,9 +58,7 @@ class _Header extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
         const SizedBox(height: 8),
-        Text(
-          Str.of(context).forgotPasswordMessage,
-        )
+        Text(Str.of(context).forgotPasswordMessage)
       ],
     );
   }
--- lib/presentation/screen/forgot_password/forgot_password_screen.dart
+++ lib/presentation/screen/forgot_password/forgot_password_screen.dart
@@ -2,27 +2,17 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:get_it/get_it.dart';
 
 import '../../../domain/bloc/forgot_password/forgot_password_bloc.dart';
 import '../../../domain/service/auth_service.dart';
-import '../../component/app_bar_with_logo.dart';
-import '../../component/big_button_component.dart';
 import '../../component/bloc_with_status_listener_component.dart';
-import '../../component/text/headline_text_components.dart';
-import '../../component/text_field_component.dart';
-import '../../config/body_sizes.dart';
 import '../../service/dialog_service.dart';
 import '../../service/navigator_service.dart';
-import '../../service/utils.dart';
-
-part 'forgot_password_content.dart';
+import 'forgot_password_content.dart';
 
 @RoutePage()
 class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({
-    super.key,
-  });
+  const ForgotPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
