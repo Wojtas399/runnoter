d4e6203ef038a09334a0509095c3209c4a69066b
Wojtas399 <wojtekp26@gmail.com>
2023-07-20 12:15:39 +0200

    Repaired initialize process on workout creator screen.

--- lib/presentation/screen/workout_creator/workout_creator_content.dart
+++ lib/presentation/screen/workout_creator/workout_creator_content.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../../../domain/additional_model/bloc_status.dart';
 import '../../../domain/bloc/workout_creator/workout_creator_bloc.dart';
 import '../../../domain/entity/workout.dart';
 import '../../component/big_button_component.dart';
 import '../../component/body/medium_body_component.dart';
+import '../../component/loading_info_component.dart';
 import '../../component/text_field_component.dart';
 import '../../service/dialog_service.dart';
 import '../../service/utils.dart';
@@ -37,15 +39,7 @@ class WorkoutCreatorContent extends StatelessWidget {
                 child: Container(
                   color: Colors.transparent,
                   padding: const EdgeInsets.all(24),
-                  child: const Column(
-                    children: [
-                      _WorkoutName(),
-                      SizedBox(height: 24),
-                      WorkoutCreatorWorkoutStages(),
-                      SizedBox(height: 40),
-                      _SubmitButton(),
-                    ],
-                  ),
+                  child: const _Form(),
                 ),
               ),
             ),
@@ -69,6 +63,29 @@ class _AppBarTitle extends StatelessWidget {
           ? Str.of(context).workoutCreatorNewWorkoutTitle
           : Str.of(context).workoutCreatorEditWorkoutTitle,
     );
+  }
+}
+
+class _Form extends StatelessWidget {
+  const _Form();
+
+  @override
+  Widget build(BuildContext context) {
+    final BlocStatus blocStatus = context.select(
+      (WorkoutCreatorBloc bloc) => bloc.state.status,
+    );
+
+    return blocStatus is BlocStatusInitial
+        ? const LoadingInfo()
+        : const Column(
+            children: [
+              _WorkoutName(),
+              SizedBox(height: 24),
+              WorkoutCreatorWorkoutStages(),
+              SizedBox(height: 40),
+              _SubmitButton(),
+            ],
+          );
   }
 }
 
