7cb56ad11e7a79b757b65d1c95eaac6c11c79579
Wojtas399 <wojtekp26@gmail.com>
2023-08-05 20:00:00 +0200

    Tests for function to create json to update user model.

--- firebase/test/model/user_dto_test.dart
+++ firebase/test/model/user_dto_test.dart
@@ -6,25 +6,26 @@ void main() {
   const Gender gender = Gender.male;
   const String name = 'Jack';
   const String surname = 'Gadovsky';
-  const UserDto userDto = UserDto(
-    id: id,
-    gender: gender,
-    name: name,
-    surname: surname,
-  );
-  final Map<String, dynamic> userJson = {
-    'gender': gender.name,
-    'name': name,
-    'surname': surname,
-  };
 
   test(
     'from json, '
     'should map json to dto model',
     () {
-      final UserDto dto = UserDto.fromJson(id, userJson);
+      final Map<String, dynamic> json = {
+        'gender': gender.name,
+        'name': name,
+        'surname': surname,
+      };
+      const UserDto expectedDto = UserDto(
+        id: id,
+        gender: gender,
+        name: name,
+        surname: surname,
+      );
 
-      expect(dto, userDto);
+      final UserDto dto = UserDto.fromJson(id, json);
+
+      expect(dto, expectedDto);
     },
   );
 
@@ -32,9 +33,78 @@ void main() {
     'to json, '
     'should map dto model to json',
     () {
-      final Map<String, dynamic> json = userDto.toJson();
+      const UserDto dto = UserDto(
+        id: id,
+        gender: gender,
+        name: name,
+        surname: surname,
+      );
+      final Map<String, dynamic> expectedJson = {
+        'gender': gender.name,
+        'name': name,
+        'surname': surname,
+      };
 
-      expect(json, userJson);
+      final Map<String, dynamic> json = dto.toJson();
+
+      expect(json, expectedJson);
+    },
+  );
+
+  test(
+    'create json to update, '
+    'gender is null, '
+    'should not include gender in json',
+    () {
+      final Map<String, dynamic> expectedJson = {
+        'name': name,
+        'surname': surname,
+      };
+
+      final Map<String, dynamic> json = createUserJsonToUpdate(
+        name: name,
+        surname: surname,
+      );
+
+      expect(json, expectedJson);
+    },
+  );
+
+  test(
+    'create json to update, '
+    'name is null, '
+    'should not include name in json',
+    () {
+      final Map<String, dynamic> expectedJson = {
+        'gender': gender.name,
+        'surname': surname,
+      };
+
+      final Map<String, dynamic> json = createUserJsonToUpdate(
+        gender: gender,
+        surname: surname,
+      );
+
+      expect(json, expectedJson);
+    },
+  );
+
+  test(
+    'create json to update, '
+    'surname is null, '
+    'should not include surname in json',
+    () {
+      final Map<String, dynamic> expectedJson = {
+        'gender': gender.name,
+        'name': name,
+      };
+
+      final Map<String, dynamic> json = createUserJsonToUpdate(
+        gender: gender,
+        name: name,
+      );
+
+      expect(json, expectedJson);
     },
   );
 }
