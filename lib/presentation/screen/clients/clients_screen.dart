2e4b6f95a09fa625a6ad7968d4650cfe6515073a
Wojtas399 <wojtekp26@gmail.com>
2023-08-08 12:15:48 +0200

    Loading logged user's clients to clients screen.

--- lib/presentation/screen/clients/clients_screen.dart
+++ lib/presentation/screen/clients/clients_screen.dart
@@ -1,5 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../domain/bloc/clients/clients_cubit.dart';
+import '../../component/empty_content_info_component.dart';
 
 @RoutePage()
 class ClientsScreen extends StatelessWidget {
@@ -7,6 +11,32 @@ class ClientsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Clients screen');
+    return BlocProvider(
+      create: (_) => ClientsCubit()..initialize(),
+      child: const _Clients(),
+    );
+  }
+}
+
+class _Clients extends StatelessWidget {
+  const _Clients();
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Client>? clients = context.select(
+      (ClientsCubit cubit) => cubit.state,
+    );
+
+    return switch (clients) {
+      null => const CircularProgressIndicator(),
+      [] => const EmptyContentInfo(title: 'No clients'),
+      [...] => ListView.builder(
+          itemCount: clients.length,
+          itemBuilder: (_, int itemIndex) {
+            final Client client = clients[itemIndex];
+            return Text('${client.name} ${client.surname}');
+          },
+        ),
+    };
   }
 }
