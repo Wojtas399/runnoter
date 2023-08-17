b76b9b95448ba87dd224466492bd1381a8db3ae1
Wojtas399 <wojtekp26@gmail.com>
2023-08-17 13:01:53 +0200

    Client state.

---
+++ lib/domain/bloc/client/client_state.dart
@@ -0,0 +1,37 @@
+import '../../additional_model/bloc_state.dart';
+import '../../additional_model/bloc_status.dart';
+import '../../entity/user.dart';
+
+class ClientState extends BlocState<ClientState> {
+  final Gender? gender;
+  final String? name;
+  final String? surname;
+  final String? email;
+
+  const ClientState({
+    required super.status,
+    this.gender,
+    this.name,
+    this.surname,
+    this.email,
+  });
+
+  @override
+  List<Object?> get props => [status, gender, name, surname];
+
+  @override
+  ClientState copyWith({
+    BlocStatus? status,
+    Gender? gender,
+    String? name,
+    String? surname,
+    String? email,
+  }) =>
+      ClientState(
+        status: status ?? const BlocStatusComplete(),
+        gender: gender ?? this.gender,
+        name: name ?? this.name,
+        surname: surname ?? this.surname,
+        email: email ?? this.email,
+      );
+}
