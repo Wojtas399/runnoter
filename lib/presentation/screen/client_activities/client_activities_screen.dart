c7aff783bd6a0ddb11fa83a3005ae535ad4a1298
Wojtas399 <wojtekp26@gmail.com>
2023-08-19 07:21:09 +0200

    Managing calendar tap events on client activities screen.

--- lib/presentation/screen/client_activities/client_activities_screen.dart
+++ lib/presentation/screen/client_activities/client_activities_screen.dart
@@ -2,6 +2,12 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 import '../../component/calendar/calendar_component.dart';
+import '../../config/navigation/router.dart';
+import '../../dialog/day_preview/day_preview_dialog.dart';
+import '../../dialog/day_preview/day_preview_dialog_actions.dart';
+import '../../formatter/date_formatter.dart';
+import '../../service/dialog_service.dart';
+import '../../service/navigator_service.dart';
 
 @RoutePage()
 class ClientActivitiesScreen extends StatelessWidget {
@@ -9,9 +15,53 @@ class ClientActivitiesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: Calendar(workouts: [], races: []),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Calendar(
+        workouts: const [],
+        races: const [],
+        onWorkoutPressed: _navigateToWorkout,
+        onRacePressed: _navigateToRace,
+        onAddWorkout: _navigateToWorkoutCreator,
+        onAddRace: _navigateToRaceCreator,
+        onDayPressed: (DateTime date) => _onDayPressed(context, date),
+      ),
     );
+  }
+
+  void _navigateToWorkout(String workoutId) {
+    navigateTo(WorkoutPreviewRoute(workoutId: workoutId));
+  }
+
+  void _navigateToRace(String raceId) {
+    navigateTo(RacePreviewRoute(raceId: raceId));
+  }
+
+  void _navigateToWorkoutCreator(DateTime date) {
+    navigateTo(WorkoutCreatorRoute(date: date.toPathFormat()));
+  }
+
+  void _navigateToRaceCreator(DateTime date) {
+    navigateTo(RaceCreatorRoute(dateStr: date.toPathFormat()));
+  }
+
+  Future<void> _onDayPressed(BuildContext context, DateTime date) async {
+    final DayPreviewDialogAction? action =
+        await showDialogDependingOnScreenSize(DayPreviewDialog(date: date));
+    if (action == null) return;
+    switch (action) {
+      case DayPreviewDialogActionAddWorkout():
+        _navigateToWorkoutCreator(action.date);
+        break;
+      case DayPreviewDialogActionAddRace():
+        _navigateToRaceCreator(action.date);
+        break;
+      case DayPreviewDialogActionShowWorkout():
+        _navigateToWorkout(action.workoutId);
+        break;
+      case DayPreviewDialogActionShowRace():
+        _navigateToRace(action.raceId);
+        break;
+    }
   }
 }
