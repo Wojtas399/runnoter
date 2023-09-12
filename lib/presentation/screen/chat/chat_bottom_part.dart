74fff2d680b854391bf56122401c6dbb1c136605
Wojtas399 <wojtekp26@gmail.com>
2023-09-12 10:16:54 +0200

    Displaying images in chat input.

--- lib/presentation/screen/chat/chat_bottom_part.dart
+++ lib/presentation/screen/chat/chat_bottom_part.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../domain/additional_model/cubit_status.dart';
 import '../../../domain/cubit/chat/chat_cubit.dart';
-import 'chat_message_input.dart';
+import '../../component/dialog/actions_dialog_component.dart';
+import '../../service/dialog_service.dart';
+import '../../service/image_service.dart';
+import '../../service/utils.dart';
+import 'chat_message_images.dart';
 
 class ChatBottomPart extends StatelessWidget {
   final TextEditingController _messageController = TextEditingController();
@@ -19,12 +25,31 @@ class ChatBottomPart extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.fromLTRB(16, 16, 8, 24),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Expanded(
-                child: ChatMessageInput(
-                  messageController: _messageController,
-                  onSubmitted: () => _onSubmit(context),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surfaceVariant,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    children: [
+                      const ChatMessageImages(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const _ImageButton(),
+                          Expanded(
+                            child: _MessageInput(
+                              messageController: _messageController,
+                              onSubmitted: () => _onSubmit(context),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
               _SubmitButton(
@@ -40,6 +65,110 @@ class ChatBottomPart extends StatelessWidget {
   Future<void> _onSubmit(BuildContext context) async {
     await context.read<ChatCubit>().submitMessage();
     _messageController.clear();
+  }
+}
+
+class _ImageButton extends StatefulWidget {
+  const _ImageButton();
+
+  @override
+  State<StatefulWidget> createState() => _ImageButtonState();
+}
+
+class _ImageButtonState extends State<_ImageButton> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: _onImagePressed,
+      icon: const Icon(Icons.image_outlined),
+    );
+  }
+
+  Future<void> _onImagePressed() async {
+    final ImageSource? imageSource =
+        kIsWeb ? ImageSource.gallery : await _askForImageSource();
+    if (imageSource == null) return;
+    switch (imageSource) {
+      case ImageSource.gallery:
+        await _pickImagesFromGallery();
+      case ImageSource.camera:
+        await _capturePhotoFromCamera();
+    }
+  }
+
+  Future<ImageSource?> _askForImageSource() async => await askForAction(
+        actions: [
+          ActionsDialogItem(
+            icon: const Icon(Icons.photo_library),
+            label: Str.of(context).chatPickImagesFromGallery,
+            value: ImageSource.gallery,
+          ),
+          ActionsDialogItem(
+            icon: const Icon(Icons.camera_alt),
+            label: Str.of(context).chatCapturePhotoFromCamera,
+            value: ImageSource.camera,
+          ),
+        ],
+      );
+
+  Future<void> _pickImagesFromGallery() async {
+    final ChatCubit chatCubit = context.read<ChatCubit>();
+    final List<Uint8List> images = await pickMultipleImages();
+    if (images.isNotEmpty) chatCubit.imagesToSendAdded(images);
+  }
+
+  Future<void> _capturePhotoFromCamera() async {
+    final ChatCubit chatCubit = context.read<ChatCubit>();
+    final Uint8List? image = await capturePhoto();
+    if (image != null) chatCubit.imagesToSendAdded([image]);
+  }
+}
+
+class _MessageInput extends StatelessWidget {
+  final TextEditingController messageController;
+  final VoidCallback onSubmitted;
+
+  const _MessageInput({
+    required this.messageController,
+    required this.onSubmitted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(100),
+      borderSide: BorderSide(
+        color: Theme.of(context).colorScheme.surfaceVariant,
+      ),
+    );
+    final CubitStatus cubitStatus = context.select(
+      (ChatCubit cubit) => cubit.state.status,
+    );
+
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 200),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          border: border,
+          focusedBorder: border,
+          enabledBorder: border,
+          disabledBorder: border,
+          errorBorder: border,
+          hintText: Str.of(context).chatWriteMessage,
+          contentPadding: const EdgeInsets.fromLTRB(0, 8, 16, 8),
+          counterText: '',
+        ),
+        enabled: cubitStatus is! CubitStatusLoading,
+        maxLength: 400,
+        maxLines: null,
+        textInputAction: TextInputAction.send,
+        controller: messageController,
+        onChanged: context.read<ChatCubit>().messageChanged,
+        onTapOutside: (_) => unfocusInputs(),
+        onSubmitted: (_) => onSubmitted(),
+      ),
+    );
   }
 }
 
