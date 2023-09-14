fdbfa150f020dd1b3a480a45902f51b59d3dd158
Wojtas399 <wojtekp26@gmail.com>
2023-09-14 10:47:08 +0200

    No messages info in chat.

--- lib/presentation/screen/chat/chat_messages.dart
+++ lib/presentation/screen/chat/chat_messages.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../../common/date_service.dart';
 import '../../../dependency_injection.dart';
 import '../../../domain/cubit/chat/chat_cubit.dart';
 import '../../../domain/entity/message.dart';
+import '../../component/empty_content_info_component.dart';
 import '../../component/gap/gap_horizontal_components.dart';
 import '../../component/loading_info_component.dart';
+import '../../component/padding/paddings_24.dart';
 import '../../component/text/label_text_components.dart';
 import '../../extension/context_extensions.dart';
 import '../../formatter/date_formatter.dart';
@@ -27,46 +30,59 @@ class ChatMessages extends StatelessWidget {
       (ChatCubit cubit) => cubit.state.messagesFromLatest,
     );
 
-    return messages == null || messages.isEmpty || loggedUserId == null
+    return messages == null || loggedUserId == null
         ? const LoadingInfo()
-        : LayoutBuilder(
-            builder: (context, constraints) {
-              final double maxMessageWidth = constraints.maxWidth * 0.6;
+        : switch (messages) {
+            [] => Paddings24(
+                child: EmptyContentInfo(
+                  icon: Icons.message,
+                  title: Str.of(context).chatNoMessagesInfoTitle,
+                  subtitle: Str.of(context).chatNoMessagesInfoMessage,
+                ),
+              ),
+            [...] => LayoutBuilder(
+                builder: (context, constraints) {
+                  final double maxMessageWidth = constraints.maxWidth * 0.6;
 
-              return ListView.separated(
-                reverse: true,
-                padding: const EdgeInsets.all(16),
-                itemCount: messages.length + 1,
-                controller: scrollController,
-                separatorBuilder: (_, int itemIndex) {
-                  if (itemIndex >= messages.length - 1) return const SizedBox();
-                  final Message currentMessage = messages[itemIndex];
-                  final Message nextMessage = messages[itemIndex + 1];
-                  final bool areDifferentDays = !_dateService.areDaysTheSame(
-                    currentMessage.dateTime,
-                    nextMessage.dateTime,
+                  return ListView.separated(
+                    reverse: true,
+                    padding: const EdgeInsets.all(16),
+                    itemCount: messages.length + 1,
+                    controller: scrollController,
+                    separatorBuilder: (_, int itemIndex) {
+                      if (itemIndex >= messages.length - 1) {
+                        return const SizedBox();
+                      }
+                      final Message currentMessage = messages[itemIndex];
+                      final Message nextMessage = messages[itemIndex + 1];
+                      final bool areDifferentDays =
+                          !_dateService.areDaysTheSame(
+                        currentMessage.dateTime,
+                        nextMessage.dateTime,
+                      );
+                      return areDifferentDays
+                          ? _DaySeparator(date: currentMessage.dateTime)
+                          : const SizedBox();
+                    },
+                    itemBuilder: (_, int messageIndex) {
+                      if (messageIndex == messages.length) {
+                        final Message previousMessage =
+                            messages[messageIndex - 1];
+                        return _DaySeparator(date: previousMessage.dateTime);
+                      }
+                      final Message currentMessage = messages[messageIndex];
+                      return ChatMessageItem(
+                        maxWidth: maxMessageWidth,
+                        isSender: loggedUserId == currentMessage.senderId,
+                        text: currentMessage.text,
+                        images: currentMessage.images,
+                        dateTime: currentMessage.dateTime,
+                      );
+                    },
                   );
-                  return areDifferentDays
-                      ? _DaySeparator(date: currentMessage.dateTime)
-                      : const SizedBox();
                 },
-                itemBuilder: (_, int messageIndex) {
-                  if (messageIndex == messages.length) {
-                    final Message previousMessage = messages[messageIndex - 1];
-                    return _DaySeparator(date: previousMessage.dateTime);
-                  }
-                  final Message currentMessage = messages[messageIndex];
-                  return ChatMessageItem(
-                    maxWidth: maxMessageWidth,
-                    isSender: loggedUserId == currentMessage.senderId,
-                    text: currentMessage.text,
-                    images: currentMessage.images,
-                    dateTime: currentMessage.dateTime,
-                  );
-                },
-              );
-            },
-          );
+              ),
+          };
   }
 }
 
