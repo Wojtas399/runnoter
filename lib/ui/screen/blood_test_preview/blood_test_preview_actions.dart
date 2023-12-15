9898d81c652c01ec80327af2d8aa055679e4d329
Wojtas399 <wojtekp26@gmail.com>
2023-12-15 14:42:49 +0100

    Correct working of back navigation in blood test preview.

--- lib/ui/screen/blood_test_preview/blood_test_preview_actions.dart
+++ lib/ui/screen/blood_test_preview/blood_test_preview_actions.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -38,7 +39,12 @@ class BloodTestPreviewActions extends StatelessWidget {
       showLoadingDialog();
       await cubit.deleteTest();
       closeLoadingDialog();
-      navigateBack();
+      if (context.mounted) {
+        context.router.popUntil((route) {
+          final String? routeName = route.settings.name;
+          return routeName == HomeRoute.name || routeName == ClientRoute.name;
+        });
+      }
       showSnackbarMessage(str.bloodTestPreviewDeletedTestMessage);
     }
   }
